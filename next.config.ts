855e041bf022ac22d700b6173905df2bd140a280
duesseldorf-koryukai <duesseldorf.koryukai@gmail.com>
2025-06-20 15:35:55 +0200

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  images: {unoptimized: true},
+  basePath: isProd ? '/homepage' : '',
+  assetPrefix: isProd ? '/homepage/': '',
+};
+
+export default nextConfig;
